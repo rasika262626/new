c713ed48c66a1d5803d0b92c09dd0d52dae1ddbb
Rasika Dumbre <123734486+rasika262626@users.noreply.github.com>
2026-03-02 14:24:29 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String[] args){
         System.out.println("Welcome to Hello world");
-        System.out.println("Numbers addition: "+(6000+60));
+        System.out.println("Numbers addition: "+(500+60));
     }
 }
