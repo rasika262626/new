9566875e1456df1e7f5421ec508eb00f567b83cf
Rasika Dumbre <123734486+rasika262626@users.noreply.github.com>
2026-03-07 22:32:04 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String[] args){
-        System.out.println("Welcome to Jenkins");
+        System.out.println("edited poll scm");
         System.out.println("Numbers addition: "+(700+60));
     }
 }
