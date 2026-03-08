939adad56b5c52a296eabcc42cc90cf0175b156b
Rasika Dumbre <123734486+rasika262626@users.noreply.github.com>
2026-03-08 19:59:45 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String[] args){
-        System.out.println("New Edited the java file");
+        System.out.println("update for webhook trigger");
         System.out.println("Numbers addition: "+(500+60));
     }
 }
