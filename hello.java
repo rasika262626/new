ee3d814fb29a42caffd46a7fc5ebfcfd217e2958
Rasika Dumbre <123734486+rasika262626@users.noreply.github.com>
2026-03-08 19:24:07 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String[] args){
-        System.out.println("Edited the java file");
-        System.out.println("Numbers addition: "+(800+60));
+        System.out.println("New Edited the java file");
+        System.out.println("Numbers addition: "+(500+60));
     }
 }
